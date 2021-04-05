78e12d47970376a267df89c3c5605765abe95279
Priya Bharmbe <priyabharambe70@gmail.com>
2021-04-05 21:43:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import{ FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AccessDataForm';
+  title = 'AccessForm';
+
+  info;
+  constructor(private formBuilder: FormBuilder) {
+    this.info = this.formBuilder.group({
+      fname: '',
+      lname: '',
+      email: '',
+      contact: '',
+      city:'',
+      subject:''
+    })
+  }
+
+  onSubmit(values: any){
+    console.warn("Form Submit", values)
+    }
+
 }
